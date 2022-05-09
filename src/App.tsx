c7241c19191459a3085d23bbdc2f10cd52b6fad4
J.Javier Reig <jreigpas@nttdata.com>
2022-05-09 20:10:00 +0200

    fix message

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { nativeAlert } from "@tef-novum/webview-bridge";
 
 function App() {
+
+  let window: any;
   const myfunction = () => {
     nativeAlert({
       message: "Purchase completed!",
@@ -14,10 +16,17 @@ function App() {
     });
   };
 
+  const sendDataToReactNativeApp = async () => {
+    //const window: any;
+        window.ReactNativeWebView.postMessage('Data from WebView / Website');
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <button className="w3-button w3-blue" onClick={myfunction}>Click Me!</button>
+        <button className="w3-button w3-blue" onClick={sendDataToReactNativeApp}>Click Me!2</button>
       </header>
     </div>
   );
