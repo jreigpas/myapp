457a0a763a5211f769fe9a55667bb3305b443581
J.Javier Reig <jreigpas@nttdata.com>
2022-05-09 17:46:18 +0200

    adding a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {nativeAlert} from '@tef-novum/webview-bridge';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { nativeAlert } from "@tef-novum/webview-bridge";
 
 function App() {
-
   const myfunction = () => {
-
     nativeAlert({
-      message: 'Purchase completed!',
-      title: 'Ok!',
-      buttonText: 'prueba'
-  }).then((res) => {
-      console.log('alert closed');
-  });
-
-  }
-
-  myfunction();
+      message: "Purchase completed!",
+      title: "Ok!",
+      buttonText: "prueba",
+    }).then((res) => {
+      console.log("alert closed");
+    });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={myfunction} />
       </header>
     </div>
   );
