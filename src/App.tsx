4274c4a139fa2e4c0412e73bcaf715344898b096
J.Javier Reig <jreigpas@nttdata.com>
2022-05-10 20:55:25 +0200

    adding a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { nativeAlert } from "@tef-novum/webview-bridge";
+import { nativeAlert, nativeConfirm } from "@tef-novum/webview-bridge";
 
 function App() {
-
   let window: any;
   const myfunction = () => {
     nativeAlert({
@@ -17,14 +16,44 @@ function App() {
   };
 
   const sendDataToReactNativeApp = async () => {
-    window.ReactNativeWebView.postMessage('Data from WebView / Website');
+    window.ReactNativeWebView.postMessage("Data from WebView / Website");
   };
+
+  
+  const callNativeConfirm = () => {
+
+    nativeConfirm({
+      title: "Confirm",
+      message: "Send message?",
+      acceptText: "Yes",
+      cancelText: "No",
+    }).then((res) => {
+      if (res) {
+        console.log("message sent");
+      }
+    });
+
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <button className="w3-button w3-blue" onClick={myfunction}>Click Me!</button>
-        <button className="w3-button w3-blue" onClick={sendDataToReactNativeApp}>Click Me!2</button>
+        <button className="w3-button w3-blue" onClick={myfunction}>
+          Click Me!
+        </button>
+        <button
+          className="w3-button w3-blue"
+          onClick={sendDataToReactNativeApp}
+        >
+          Click Me!2
+        </button>
+        <button
+          className="w3-button w3-blue"
+          onClick={callNativeConfirm}
+        >
+          Click Me!3
+        </button>
       </header>
     </div>
   );
