a0c134245208a2ca648974c4fb8fa2cb93a9111e
J.Javier Reig <jreigpas@nttdata.com>
2022-05-19 15:22:56 +0200

    format component

--- src/App.js
+++ src/App.js
@@ -2,41 +2,24 @@
 import "./App.css";
 import { useState } from "react";
 
-// window.changeText = function(url) {
-//   console.log('llega 1');
-//   alert('valor');
-
-// };
-
-
 function App() {
-
   const [mensaje, setMensaje] = useState("sin mensaje");
 
-  const getFromAndroid = () => { // eslint-disable-line
+  const getFromAndroid = () => {
+    // eslint-disable-line
 
-      const myVar = Android.getFromAndroid("informacion desde React"); // eslint-disable-line
+    const myVar = Android.getFromAndroid("informacion desde React"); // eslint-disable-line
+  };
 
-  }
-
-//   const changeText = (msg ) => { // eslint-disable-line
-//     console.log('llega 1q');
-//     setMensaje(msg);
-
-// }
-
-    window.changeText = function(msg) {
-      console.log('llega 1');
-      setMensaje(msg);
-      alert('valor');
-
-    };
-
+  window.changeText = function (msg) {
+    console.log("llega 1");
+    setMensaje(msg);
+    alert("valor");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-
         <p>este es el mensaje: {mensaje}</p>
 
         <button
@@ -45,8 +28,6 @@ function App() {
         >
           from React
         </button>
-
-
       </header>
     </div>
   );
