d3c93c5a215cdf0512d13e28e9301a4683c5f0df
J.Javier Reig <jreigpas@nttdata.com>
2022-05-26 19:28:27 +0200

    new route

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,14 @@ import Dashboard from "./components/dashboard/Dashboard";
 
 
 function App() {
-  const [mensaje, setMensaje] = useState("--");
-  const [mensajeEnviar, setMensajeEnviar] = useState("sin mensaje");
 
-  const getFromReact = () => {
-      console.log('llega a mensaje enviar '+ mensajeEnviar);
-      window.Native.getFromReact(mensajeEnviar);
-  };
-
-  const handleInputChange = (event: any) => {
-    console.log('valor:'+event.target.value);
-    setMensajeEnviar(event.target.value);
-  };
-
-
-  window.getFromNative = function (msg: string) {
-    console.log("llega 1");
-    setMensaje(msg);
-    alert("valor");
-  };
 
   return (
 <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
       <Route path="/" element={<Home />} />
-      <Route path="myApp/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Home />} />
     </Routes>
     </BrowserRouter>
